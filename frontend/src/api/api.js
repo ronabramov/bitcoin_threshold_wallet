652bf2378941fa8f21f06742ca3257e178c9b99a
RonAbramovich <Ronabram@post.bgu.ac.il>
2025-01-07 15:49:18 +0200

    allow FE server to address BE

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_URL = "https://bitcoin-threshold-wallet.onrender.com"; // Update with your backend URL
+const API_URL = "https://bitcoin-threshold-wallet.onrender.com"; // render-url
 
 // Fetch wallets for a user
 export const getWallets = async (userId) => {
-    const response = await axios.get(`${API_URL}/wallets/${userId}`);
+    const response = await axios.get(`${API_URL}/wallets/wallets/${userId}`);
     return response.data;
 };
 
 // Create a new transaction
 export const createTransaction = async (data) => {
-    const response = await axios.post(`${API_URL}/transactions/request`, data);
+    const response = await axios.post(`${API_URL}/transactions/transactions/request`, data);
     return response.data;
 };
