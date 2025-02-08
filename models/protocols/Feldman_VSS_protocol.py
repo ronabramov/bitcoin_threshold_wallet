72cd5c29fb547ed257f82ad01f516a50d06419ef
RonAbramovich <Ronabram@post.bgu.ac.il>
2025-02-08 21:58:54 +0200

    class Feldman VSS protocol Defined, getting as argument curve and parameters.

--- models/protocols/Feldman_VSS_protocol.py
+++ models/protocols/Feldman_VSS_protocol.py
@@ -1,45 +1,53 @@
-from ecdsa import NIST256p
-from ecdsa.ellipticcurve import Point
+from ecdsa import NIST256p, curves
 import random
 
-def generate_coefficients(secret, t, curve):
-    return [secret] + [random.randint(1, curve.order - 1) for _ in range(t)]
+"""
+Feldman VSS Protocol For Key Generation protocol
+Gets as argument curve from ecdsa library, n and t.
+After initialization, enables generating shares for secret.
+Moreover, given shares, enables verification that the shares are valid  
+"""
 
-def compute_v_i(coeffs, G):
-    #  The operation in the ecdsa lib over a generator g is denote by sum
-    #  That is g^a_i = a_i *g
-    return [coeff * G for coeff in coeffs]
+class Feldman_VSS_Protocol:
+    def __init__(self, curve : curves.Curve, n : int, t: int):
+        self.curve = curve
+        self.G = curve.generator
+        self.n =n
+        self.t =t
 
-def evaluate_polynomial(x, coeffs, curve):
-    return sum(coeff * (x ** i) for i, coeff in enumerate(coeffs))
+    def generate_coefficients(self, secret):
+        return [secret] + [random.randint(1, curve.order - 1) for _ in range(t)]
 
-def generate_shares(n, t, secret, curve):
-    # Using the notation from the paper, v_i := g^a_i. 
-    # In the Feldman VSS, in addition to a share, every player gets {v_i}_i=1 ^t.
-    # In addition every player gets v_0 = g^secret = g^a_0 
-    G = curve.generator
-    coeffs = generate_coefficients(secret, t, curve)
-    v_i = compute_v_i(coeffs, G)
-    g_secret = v_i[0]
-    shares = [{
-        'index': i,
-        'p(i)': evaluate_polynomial(i, coeffs, curve),
-        'v_i': v_i,
-        'g^secret': g_secret
-    } for i in range(1, n+1)]
-    return shares
+    def compute_v_i(self, coeffs):
+        #  The operation in the ecdsa lib over a generator g is denote by sum
+        #  That is g^a_i = a_i *g
+        return [coeff * self.G for coeff in coeffs]
 
-def verify_share(share, G, curve):
-    g_p_i = share['p(i)'] * G # g^p(i) should be = product (e.g sum) og the shares g^a_j ^ (i^j)
-    product = 0 * G  # Identity element
-    for j, v in enumerate(share['v_i']):
-        product += (share['index'] ** j) * v
-    return g_p_i == product
+    def evaluate_polynomial(self, x, coeffs):
+        return sum(coeff * (x ** i) for i, coeff in enumerate(coeffs))
 
-def run_protocol(n, t, secret):
-    ""
-    "Args: n= # users of the "
-    ""
+    def generate_shares(self, secret):
+        # Using the notation from the paper, v_i := g^a_i. 
+        # In the Feldman VSS, in addition to a share, every player gets {v_i}_i=1 ^t.
+        # In addition every player gets v_0 = g^secret = g^a_0 
+        coeffs = self.generate_coefficients(secret)
+        v_i = self.compute_v_i(coeffs)
+        g_secret = v_i[0]
+        shares = [{
+            'index': i,
+            'p(i)': self.evaluate_polynomial(i, coeffs),
+            'v_i': v_i,
+            'g^secret': g_secret
+        } for i in range(1, n+1)]
+        return shares
+
+    def verify_share(self, share):
+        G = self.G
+        g_p_i = share['p(i)'] * G # g^p(i) should be = product (e.g sum) og the shares g^a_j ^ (i^j)
+        product = 0 * G  # Identity element
+        for j, v in enumerate(share['v_i']):
+            product += (share['index'] ** j) * v
+        return g_p_i == product
 
 
 #Usage Example:
@@ -47,11 +55,14 @@ def run_protocol(n, t, secret):
 curve = NIST256p
 n = 5  # Number of participants
 t = 3  # Threshold
+protocol = Feldman_VSS_Protocol(curve=curve, n=n, t=t)
 secret = random.randint(1, curve.order - 1)
 
 # Generate shares
-shares = generate_shares(n, t, secret, curve)
+shares = protocol.generate_shares(secret)
 
 # Verify shares
-verified = [verify_share(share, curve.generator, curve) for share in shares]
+verified = [protocol.verify_share(share) for share in shares]
 print("Shares verified:", all(verified))
+
+
