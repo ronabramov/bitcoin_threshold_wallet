8228eaebc31bb37db166eb5b398ad26a79304474
RonAbramovich <Ronabram@post.bgu.ac.il>
2025-02-09 15:59:50 +0200

    Generating messages types IP - for communicating different messages

---
+++ models/messages/user_global_share.py
@@ -0,0 +1,5 @@
+class user_global_share:
+    def __init__(self, index: int, evaluation : int):
+        self.user_index = index
+        self.evaluation = evaluation
+        
--- models/models.py
+++ models/models.py
@@ -1,10 +1,13 @@
-from pydantic import BaseModel
-from typing import List, Optional
+# from pydantic import BaseModel
+# from typing import List, Optional
 
-class TransactionInfo(BaseModel):
-    wallet_id: str
-    transaction_details: dict
+# class TransactionInfo(BaseModel):
+#     wallet_id: str
+#     transaction_details: dict
 
-class Message(BaseModel):
-    recipient_id: str
-    content: str
+# class Message(BaseModel):
+#     recipient_id: str
+#     content: str
+
+# class user_signature_generation_share(BaseModel):
+
