0a822f0f5226cbdbd17c960494af112e2c847eec
RonAbramovich <Ronabram@post.bgu.ac.il>
2025-02-10 19:20:23 +0200

    added commitment algorithm implementation

---
+++ models/protocols/Commitment.py
@@ -0,0 +1,17 @@
+from ecdsa.ellipticcurve import PointJacobi
+from phe import paillier, PaillierPrivateKey, PaillierPublicKey
+
+def commit_x(public_key : PaillierPublicKey , x):
+    """
+    Using Paillier Homomorphic encryption as commitment and decommitment algorithm
+    """
+    return public_key.encrypt(x)
+
+def commit_multiple_values(values : list):
+    return [commit_x(x) for x in values]
+
+def decommit_value(private_key : PaillierPrivateKey, value):
+    return private_key.decrypt(value)
+
+
+
