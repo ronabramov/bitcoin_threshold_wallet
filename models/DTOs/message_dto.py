8849a50ca9b0189cd6deb232975809d531c1fa8a
gilad.i <gilad.i@dataloop.ai>
2025-02-19 13:57:02 +0200

    introducing mType for real

--- models/DTOs/message_dto.py
+++ models/DTOs/message_dto.py
@@ -6,13 +6,31 @@ from models.value_knowledge_zk_proof import value_knowledge_zk_proof
 from models.commitment import Commitment
 from models.protocols.MtaAndMtaWcMessages import MtaChallenge, MtaCommitmentAlice, MtaCommitmentBob, MtaProofForChallengeAlice, MtaProofForChallengeBob, MtaWcCommitmentBob
 from typing import Union
+from enum import Enum
+
+class MessageType(Enum):
+    Transaction = "transaction"
+    TransactionRequest = "transaction_request"
+    TransactionResponse = "transaction_response"
+    KeyGenerationShare = "key_generation_share"
+    UserPublicShare = "user_public_share"
+    Commitment = "commitment"
+    ValueKnowledgeZkProof = "value_knowledge_zk_proof"
+    MtaChallenge = "mta_challenge"
+    MtaCommitmentAlice = "mta_commitment_alice"
+    MtaCommitmentBob = "mta_commitment_bob"
+    MtaProofForChallengeAlice = "mta_proof_for_challenge_alice"
+    MtaProofForChallengeBob = "mta_proof_for_challenge_bob"
+    MtaWcCommitmentBob = "mta_wc_commitment_bob"
+    WalletGenerationMessage = "wallet_generation_message"
+
 
 class MessageDTO(BaseModel):
     """
     A Wrapper for messages - adding the functionality of type
     The Wallet Listener would parse every message w.r.t it's type.
     """
-    type: str
+    type: MessageType
     data: Union[TransactionDTO, TransactionResponse, key_generation_share, user_public_share,
                 Commitment, value_knowledge_zk_proof, MtaChallenge, MtaCommitmentAlice,
                   MtaCommitmentBob, MtaProofForChallengeAlice, MtaProofForChallengeBob, MtaWcCommitmentBob, WalletGenerationMessage]
