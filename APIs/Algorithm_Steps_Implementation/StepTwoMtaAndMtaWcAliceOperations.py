5d1c9ca7c0f7c49dddd31f8a57009e9506468390
gilad.i <gilad.i@dataloop.ai>
2025-03-10 22:37:16 +0200

    aligned mta alice

--- APIs/Algorithm_Steps_Implementation/StepTwoMtaAndMtaWcAliceOperations.py
+++ APIs/Algorithm_Steps_Implementation/StepTwoMtaAndMtaWcAliceOperations.py
@@ -2,7 +2,6 @@ from local_db.sql_db import Wallet, WalletUserData, Mta_As_Alice_Users_Data, Mta
 import local_db.sql_db_dal as db_dal
 from models.models import user_modulus, user_public_share, user_secret_signature_share, GPowerX
 from models.protocols.mta_protocol import MTAProtocolWithZKP
-from models.protocols.AliceZKProofModels import AliceCommitmentMessage
 from models.protocols.MtaWc_protocl import MtaWcProtocolWithZKP
 from models.protocols.BobZKProofMtAModels import Bob_ZKProof_ProverCommitment, Bob_ZKProof_Proof_For_Challenge
 from ecdsa.curves import Curve, curve_by_name
@@ -12,6 +11,7 @@ from models.DTOs.message_dto import MessageDTO
 from Services.MatrixService import MatrixService
 from phe import EncryptedNumber
 from Services.Context import Context
+from models.protocols.MtaAndMtaWcMessages import MtaCommitmentAlice
 
 
 class StepTwoMtaAndMtaWcAliceOperations:
@@ -48,7 +48,7 @@ class StepTwoMtaAndMtaWcAliceOperations:
         mta_enc_ki, mta_commitment_of_ki = mta_protocol.alice_encrypting_a_and_sending_commitment(a=k_i, alice_paillier_public_key=user_secret_data.paillier_public_key)
         insertion_success = db_dal.insert_alice_a_and_enc_a(transaction_id=transaction_id, user_index=sending_user_public_share.user_index,
                                                              counterparty_index=destination_user_public_share.user_index, a=k_i, enc_a=mta_enc_ki)
-        commitment_message = AliceCommitmentMessage(zk_proof_commitment=mta_commitment_of_ki, enc_a=mta_enc_ki)
+        commitment_message = MtaCommitmentAlice(zk_proof_commitment=mta_commitment_of_ki, enc_a=mta_enc_ki)
         if not insertion_success:
             print(f'Failed Recording encryption of k_i for target user : {destination_user_public_share.user_id}')
             raise SystemError(f'Failed Recording encryption of k_i for target user : {destination_user_public_share.user_id}')
@@ -76,7 +76,7 @@ class StepTwoMtaAndMtaWcAliceOperations:
             
         
         mta_wc_enc_ki, mta_wc_commitment_of_ki = mta_wc_protocol.alice_encrypting_a_and_sending_commitment(a=k_i, alice_paillier_public_key=user_secret_data.paillier_public_key) #Check - don't we want to save the encripted
-        commitment_message = AliceCommitmentMessage(zk_proof_commitment=mta_wc_commitment_of_ki, enc_a=mta_wc_enc_ki)
+        commitment_message = MtaCommitmentAlice(zk_proof_commitment=mta_wc_commitment_of_ki, enc_a=mta_wc_enc_ki)
 
         # Create enhanced MessageDTO with metadata
         mta_commitment_message = MessageDTO(
